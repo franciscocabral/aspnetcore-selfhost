425346bed040070e2abc014c720ea750e311380a
Josenilton Cabral | Stone <jcabral@stone.com.br>
2017-07-12 10:55:29 -0300

    feature(Template) initial template

---
+++ src/Flip.Api/Controllers/EstablishmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace flip.api.Controllers
+{
+    [Route("establishments")]
+    public class EstablishmentsController
+    {
+        // GET establishments
+        [HttpGet]      
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Pet Store", "Bodyshop" };
+        }
+    }
+}
---
+++ src/Flip.Api/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace flip.api
+{
+    /// <summary>
+    /// The startup class
+    /// 
+    /// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    /// </summary>
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
